582193a2ab58382d575e9567555ef4c29b720270
stud <stud@it233-0.almetpt.local>
2025-03-18 18:34:58 +0300

    Добавил шаблон

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Header from '$lib/components/Header.svelte';
+import {Container, Styles} from '@sveltestrap/sveltestrap';
+    const {children} = $props();
+</script>
+
+<Styles/>
+
+<Container>
+    <Header/>
+    {@render children()}
+</Container>
+
+
