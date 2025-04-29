d292954999168b1f50e4381647e36a2f5e51d193
stud <stud@it233-0.almetpt.local>
2025-04-29 18:39:34 +0300

    template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {Carousel, CarouselCaption, CarouselItem, Container, Image} from '@sveltestrap/sveltestrap';
+    import { Card, CardBody, CardFooter, CardHeader, CardImg, CardText, Col, Container, Image, Row,} from '@sveltestrap/sveltestrap';
 </script>
 
 <svelte:head>
@@ -7,15 +7,41 @@
 </svelte:head>
 
 <Container class="shadow-lg p-2">
-    <h1>Добро пожаловать на главную страницу!</h1>
-    <p>На нашем сайте можно найти крутую информацию о фреймворках и библиотеках</p>
-    <Carousel interval={3000}>
-        <CarouselItem>
-            <Image src="https://avatars.mds.yandex.net/i?id=4f62637c5162586faa035ffcd0874c4154c536d9-12471923-images-thumbs&n=13"/>
-        </CarouselItem>
-        <CarouselItem>
-            <Image src="https://avatars.mds.yandex.net/i?id=020979e16c9d1a4633baa8fc2241ccac_l-5344756-images-thumbs&n=13"/>
-        </CarouselItem>
-    </Carousel>
+    <h1 class="text-center">Добро пожаловать на главную страницу!</h1>
+    <p>Работу представляет:ФИО</p>
+    <p>Тема: </p>
+    
+    <Row>
+        <Col>
+            <Card>
+                <CardHeader>Lada2114</CardHeader>
+                <CardBody>
+                    <CardImg src="https://avatars.mds.yandex.net/get-autoru-reviews/1533577/e41103d5a99abd43f1bf03b34ebdcf33/1200x900"/>
+                    <CardText>79л.с.</CardText>
+                </CardBody>
+            </Card>
+        </Col>
+
+        <Col>
+            <Card>
+                <CardHeader>Название</CardHeader>
+                <CardBody>
+                    <CardImg src="https://avatars.mds.yandex.net/get-autoru-reviews/1533577/e41103d5a99abd43f1bf03b34ebdcf33/1200x900"/>
+                    <CardText>Lada 2114</CardText>
+                </CardBody>
+            </Card>
+        </Col>
+
+        <Col>
+            <Card>
+                <CardHeader>Название</CardHeader>
+                <CardBody>
+                    <CardImg src="https://avatars.mds.yandex.net/get-autoru-reviews/1533577/e41103d5a99abd43f1bf03b34ebdcf33/1200x900"/>
+                    <CardText>Lada 2114</CardText>
+                </CardBody>
+            </Card>
+        </Col>
+    </Row>
+
 </Container>
 
